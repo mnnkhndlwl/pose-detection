e6d4f43e12a823af165112f1245dba961984b394
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-09-12 23:17:28 +0530

    drawing all points

--- pose.js
+++ pose.js
@@ -1,7 +1,42 @@
-function setup() {
+let video;
+let poseNet;
+let results;
+let myPose;
+let skeleton;
 
+function setup() {
+    createCanvas(1280,720);
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, onReady); // video - where we wannt to use our pose net onReady - callback function
+
+    poseNet.on("pose", (res)=> { // once you get the pose then result
+        // console.log(res);
+        if(res.length){
+            results = res;
+        myPose = results[0].pose;
+        }
+    });
+}
+
+function onReady() {
+    console.log("Model is ready");
 }
 
 function draw() {
-    
+    // ellipse(80,80,60,60);
+    image(video,0,0);
+    fill(255,120,0);
+    if(myPose){
+        ellipse(myPose.nose.x,myPose.nose.y,20);
+        if(myPose.score>0.3){
+            for(let i =0;i<myPose.keypoints.length;i++){
+                let x = myPose.keypoints[i].position.x;
+                let y = myPose.keypoints[i].position.y;
+                fill(0,0,255);
+                ellipse(x,y,20);
+            }
+        }
+    }
 }
+
